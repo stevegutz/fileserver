c5cd317bc9cb43b95de3218093029e456b583a2e
Stephen Gutz <sgutz@sgutz.office.yext.com>
2015-04-27 13:07:59 -0400

    Add logging

--- main.go
+++ main.go
@@ -2,24 +2,49 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
 	"net/http"
-	"os"
 	"strconv"
 )
 
 var (
-	dir  = flag.String("d", ".", "directory to serve")
-	port = flag.Int("p", 8080, "port to serve on")
+	dir           = flag.String("d", ".", "directory to serve")
+	port          = flag.Int("p", 8080, "port to serve on")
+	enableLogging = flag.Bool("enablelogging", true, "used to enable / disable logging")
+	logFavicon    = flag.Bool("logfavicon", false, "whether to log favicon requests")
 )
 
 func main() {
 	flag.Parse()
-	fmt.Printf("Serving '%s' on port %d\n", *dir, *port)
-	perror(http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(http.Dir(*dir))))
+	log.Printf("- Serving '%s' on port %d\n", *dir, *port)
+	handler := loggingHandler{http.FileServer(http.Dir(*dir))}
+	log.Fatalln("-", http.ListenAndServe(":"+strconv.Itoa(*port), handler))
 }
 
-func perror(err error) {
-	fmt.Fprintln(os.Stderr, err)
-	os.Exit(-1)
+type loggingHandler struct {
+	h http.Handler
+}
+
+func (h loggingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	statusWriter := &statusStoringResponseWriter{ResponseWriter: w}
+	h.h.ServeHTTP(statusWriter, r)
+	switch {
+	case !*enableLogging:
+	case !*logFavicon && r.RequestURI == "/favicon.ico":
+	default:
+		log.Printf("- %d %s %s%s", statusWriter.status, r.Method, r.Host, r.RequestURI)
+	}
+}
+
+type statusStoringResponseWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (w *statusStoringResponseWriter) WriteHeader(n int) {
+	w.ResponseWriter.WriteHeader(n)
+	// Attempt to handle multiple WriteHeader() calls
+	if w.status == 0 {
+		w.status = n
+	}
 }
